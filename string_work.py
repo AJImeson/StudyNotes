cd1505625cc1fc6960665b8db51ffea7ead9415a
axel.imeson@gmail.com <axel.imeson@gmail.com>
2025-10-24 12:11:46 +0200

    Update

--- string_work.py
+++ string_work.py
@@ -3,3 +3,17 @@ masked_password = "*" * len(password)
 
 print(password)
 print(len(masked_password))
+
+password = "Cybersecurity"
+first_char = password[0:5]
+print(first_char)
+
+filename = "cmdlop.exe"
+
+if filename[-4] == ".exe":
+    print("Malware found")
+    
+text = "Cyber Security"
+
+words = text.split() 
+print (words)
