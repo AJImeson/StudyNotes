7845006e880f0b953b9524324e76b114b3552549
axel.imeson@gmail.com <axel.imeson@gmail.com>
2025-10-24 10:51:09 +0200

    Update

---
+++ equations.py
@@ -0,0 +1,34 @@
+
+x = 4
+y = 6.3
+
+print(int(x+y))
+print(float(x+y))
+
+x = 5 
+y = 10
+
+print(x== y)
+print (x<y)
+
+a = True
+b = False
+
+print(a and b)
+print(a or b)
+print(not a )
+
+a = 10
+b = 3 
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+
+x = 10
+x += 5 
+
+print(x)
---
+++ string_work.py
@@ -0,0 +1,5 @@
+password = "swordfish123"
+masked_password = "*" * len(password)
+
+print(password)
+print(len(masked_password))
